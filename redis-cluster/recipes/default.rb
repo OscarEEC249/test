381ae47ff46a5ea81fa27802f52d09227fcbcf1e
Oscar Escobar <oscareec3979@outlook.com>
2018-04-26 14:24:48 -0600

    Adding execute bash

--- redis-cluster/recipes/default.rb
+++ redis-cluster/recipes/default.rb
@@ -5,3 +5,7 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 include_recipe 'redis::install_from_package'
+
+execute 'apache_configtest' do
+    command '/usr/sbin/apachectl configtest'
+  end

8225e6121d322740ddc332f0601e5768d1d9dfaa
Oscar Escobar <oscareec3979@outlook.com>
2018-04-26 13:12:46 -0600

    New recipe

---
+++ redis-cluster/recipes/setup.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook:: redis-cluster
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+directory '/cheftemp' do
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+  end
+  
+  
+  file '/cheftemp/testfile' do
+    content 'testcontent'
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+  end

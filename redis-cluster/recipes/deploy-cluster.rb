1cef0957f9d39c77dfaa5cb6b5a7a521ead34bf3
Oscar Escobar <oscareec3979@outlook.com>
2018-04-30 16:29:39 -0600

    Cluster

--- redis-cluster/recipes/deploy-cluster.rb
+++ redis-cluster/recipes/deploy-cluster.rb
@@ -12,8 +12,8 @@ if instances.count == 6
   ips_segment.chomp(' ')
   setup_cmd = '/usr/local/share/redis/src/redis-trib.rb create --replicas 1 '
   setup_cmd.concat(ips_segment)
-  
-  if this.instance['instance_id'] == 0
+
+  if this_instance['instance_id'] == 0
     Chef::Log.info("********** Command to execute: '#{setup_cmd}' **********")
     bash 'deploy-cluster' do
       code <<-EOH
